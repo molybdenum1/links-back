5633fb713e0e7dd6a70019aab1f13d38b2dd56b8
molybdenum1 <kozelden4ik@gmail.com>
2023-07-16 23:55:03 +0300

    creating links controller

---
+++ src/link/link.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { LinkService } from './link.service';
+import { Link } from './entity/link.entity';
+
+@Controller('links')
+export class LinkController {
+  constructor(private linkService: LinkService) {}
+
+  @Get()
+  async getAll(): Promise<Link[]> {
+    return this.linkService.getAll();
+  }
+  @Post()
+  async create(@Body() link: Link): Promise<Link> {
+    return this.linkService.create(link);
+  }
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() link: Partial<Link>) {
+    return this.linkService.update(id, link);
+  }
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    return this.linkService.delete(id);
+  }
+}
